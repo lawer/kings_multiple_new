58e291cfe516af74e1e17828a14753db36a14ea4
Carles Gonzàlez Montes <carlesgm@MacBook-Pro-de-Carles.local>
2023-02-07 13:57:24 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      teams: []
+    }
+  },
+
+  async mounted() {
+    const result = await fetch("https://api.codetabs.com/v1/proxy/?quest=https://api.kingsleague.dev/teams");
+    const teams = await result.json();
+
+    this.teams = teams;
+  }
+}
+
+</script>
+
+<template>
+  <h1>Equipos</h1>
+
+  <div>
+    <ul>
+      <li v-for="team in teams" :key="team.id">
+        <RouterLink :to="'/team/' + team.id">
+          <img :src="team.image" :alt="team.name" width="200">
+        </RouterLink>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/views/TeamView.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>{{team.name}}</h1>
+
+  <h2>Coach: {{team.coach}}</h2>
+  <div>
+    <ul>
+      <li v-for="player in team.players" :key="player.id">
+        {{ player }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: null,
+      team: {}
+    }
+  },
+  async mounted() {
+    this.id = this.$route.params.id;
+
+    const result = await fetch("https://api.codetabs.com/v1/proxy/?quest=https://api.kingsleague.dev/teams/" + this.id);
+    const team = await result.json();
+
+    this.team = team;
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
